c35a0ed6c975c9c13b24ae0b75a1e569237293cc
Artem Kurbatov <akurbatov.work@gmail.com>
2023-08-28 20:21:24 +0400

    new routs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,8 +24,41 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("Book not found")
+}
+
 func main() {
 	rounter := gin.Default()
 	rounter.GET("/books", getBooks)
+	rounter.GET("/books/:id", bookById)
+	rounter.POST("/books", createBook)
 	rounter.Run("localhost:8080")
 }
